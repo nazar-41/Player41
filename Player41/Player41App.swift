336b9588c45506395e6d905345241f70ad696e78
nazar-velkakayev <nazarwelkakayew41@gmail.com>
2024-11-09 11:49:47 +0500

    Initial Commit

---
+++ Player41/Player41App.swift
@@ -0,0 +1,17 @@
+//
+//  Player41App.swift
+//  Player41
+//
+//  Created by Nazar Velkakayev on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Player41App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
